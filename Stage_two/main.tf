e80f78befe9949ab3359be5ba92d922a3f8801d7
mstienei <mstienei@gmail.com>
2020-11-26 07:49:14 +0300

    create security group to allow ports for backend,client and mongo

--- Stage_two/main.tf
+++ Stage_two/main.tf
@@ -15,3 +15,34 @@ resource "aws_instance" "yolomy" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 }
+resource "aws_security_group" "instance" {
+  name = "yolomy-instance"
+  ingress { #frontend Port
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  ingress { #backend Port
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  ingress { #mongo Port
+    from_port   = 27018
+    to_port     = 27018
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress { #Outbound all allow
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
