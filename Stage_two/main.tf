ca19db71e6148cee0d4fe06a9a381128dc83f5bf
mstienei <mstienei@gmail.com>
2020-11-29 09:12:16 +0300

    update main terafoarm file

--- Stage_two/main.tf
+++ Stage_two/main.tf
@@ -1,27 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 2.70"
-    }
-  }
-}
 provider "aws" {
-  profile = "default"
   region  = "us-west-2"
+  shared_credentials_file = "~/.aws/credentials"
+  access_key  = var.ssh_access_key
 }
-
-resource "aws_instance" "yolomy" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-provisioner "file" {
-  source      = "/moringa/yolomy"
-  destination = "/opt/yolomy"
-}
-}
-resource "aws_security_group" "instance" {
-  name = "yolomy-instance"
+resource "aws_security_group" "yolomy" {
+  name = "yolomy_app"
   ingress { #frontend Port
     from_port   = 3000
     to_port     = 3000
@@ -58,4 +41,35 @@ resource "aws_security_group" "instance" {
     protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_instance" "yolomyapp" {
+  ami           = "ami-01ec00b4d67556905"
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  key_name                    = var.ssh_private_key
+  security_groups             = [aws_security_group.yolomy.name]
+   
+  provisioner "remote-exec" {
+  inline = ["echo 'Wait until SSH is ready'"]
+
+  connection {
+          type        = "ssh"
+          user        = "ubuntu"
+          private_key = file(var.private_key_path)
+          host        = aws_instance.yolomyapp.public_ip
+          timeout = "2m"
+          agent = false
+         }
+     }
+  provisioner "local-exec" {
+  command = "ansible-playbook  -i ${aws_instance.yolomyapp.public_ip}, --private-key ${var.private_key_path} playbook.yaml"
+        }
+  tags={
+    Name= "Yolomy App"
+  }
+}
+
+output "yolomy_ip" {
+  value = aws_instance.yolomyapp.public_ip
 }
